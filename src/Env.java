44d2caa81800a7f3d3a7fc4bd7edd462903a8d28
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-21 11:00:59 +0100

    env bdd

---
+++ src/Env.java
@@ -0,0 +1,5 @@
+final public class Env {
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/adresse?serverTimezone=UTC";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+}
