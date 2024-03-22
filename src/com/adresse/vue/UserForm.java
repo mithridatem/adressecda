8db94947c5d00fc5b58b9fdfa188c489d7d7d33c
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-22 11:33:03 +0100

    Correction Ex 34 UI Swing et bonus

--- src/com/adresse/vue/UserForm.java
+++ src/com/adresse/vue/UserForm.java
@@ -1,11 +1,17 @@
 package com.adresse.vue;
 
+import com.adresse.model.Utilisateur;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import javax.swing.text.StyleContext;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
 
-public class UserForm extends JDialog{
+
+public class UserForm extends JDialog {
     private JLabel jlName;
     private JTextField tfName;
     private JLabel jlFirstname;
@@ -19,23 +25,75 @@ public class UserForm extends JDialog{
     private JPanel jpMain;
     private JLabel jlPasswordVerif;
     private JPasswordField pfPasswordVerif;
+    private JLabel jlInfo;
 
-    public UserForm(JDialog parent){
+    public UserForm(JDialog parent) {
         super(parent);
         setTitle("Ajouter un compte utilisateur");
         setContentPane(jpMain);
-        setMaximumSize(new Dimension(800,600));
-        setMinimumSize(new Dimension(800,600));
+        setMaximumSize(new Dimension(800, 600));
+        setMinimumSize(new Dimension(800, 600));
         setVisible(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         //écouteur événement bt valider
 
         btValid.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                tfName.getText();
-                String password = String.valueOf(pfPassword.getPassword());
-                System.out.println("on a clique sur le bouton valider");
+                createUser();
+            }
+        });
+        btCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createUser();
             }
         });
     }
+
+    public void createUser() {
+        //récupérer le contenu des 5 champs de texte
+        String name = tfName.getText();
+        String firstname = tfFirstname.getText();
+        String email = tfEmail.getText();
+        String password = String.valueOf(pfPassword.getPassword());
+        String verifPassword = String.valueOf(pfPasswordVerif.getPassword());
+        //test si les 5 champs sont bien remplis
+        if (!name.isEmpty() && !firstname.isEmpty() && !email.isEmpty()) {
+            //test si les passwords correspondent
+            if (password.equals(verifPassword)) {
+                password = BCrypt.hashpw(password, BCrypt.gensalt());
+                Utilisateur user = new Utilisateur(name, firstname, email, password);
+                JOptionPane.showMessageDialog(null,
+                        "le compte a été ajouté",
+                        "Valide",
+                        JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "Les mots de passe ne correspondent pas",
+                        "Erreur",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null,
+                    "Veuillez remplir tous les champs du formulaire",
+                    "Erreur",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    public void cancelUser() {
+        //vider les champs
+        tfName.setText("");
+        tfFirstname.setText("");
+        tfEmail.setText("");
+        pfPassword.setText("");
+        pfPasswordVerif.setText("");
+        //afficher un message pour indiquer que les champs sont vidés
+        JOptionPane.showMessageDialog(null,
+                "Formulaire reset",
+                "Reset",
+                JOptionPane.INFORMATION_MESSAGE);
+        dispose();
+    }
 }
+
